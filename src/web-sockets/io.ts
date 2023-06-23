3a31fa3179e16b11eb577e09319d1c8e35830957
DIGITALWARE/EdierM <EdierM@digitalware.com.co>
2023-06-22 19:55:35 -0500

    Force connection socket

--- src/web-sockets/io.ts
+++ src/web-sockets/io.ts
@@ -2,4 +2,6 @@ import { io } from 'socket.io-client';
 
 import { WHATSAAP_API_URL } from '../config';
 
-export const socket = io(WHATSAAP_API_URL);
+export const socket = io(WHATSAAP_API_URL, {
+  forceNew: true,
+});
