3b30e8a0aef17b1fa15c64e59433b4f9913a8790
DIGITALWARE/EdierM <EdierM@digitalware.com.co>
2023-07-05 08:03:19 -0500

    remove image

--- src/dashboard/components/navbar/Nabvar.tsx
+++ src/dashboard/components/navbar/Nabvar.tsx
@@ -54,27 +54,6 @@ export function Nabvar() {
                 </svg>
               </a>
             </li>
-            <li>
-              <a
-                href='#'
-                className='nav-link nav-link-lg fullscreen-btn'
-              >
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='24'
-                  height='24'
-                  viewBox='0 0 24 24'
-                  fill='none'
-                  stroke='currentColor'
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  className='feather feather-maximize'
-                >
-                  <path d='M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3'></path>
-                </svg>
-              </a>
-            </li>
           </ul>
         </div>
         <ul className='navbar-nav navbar-right'>
