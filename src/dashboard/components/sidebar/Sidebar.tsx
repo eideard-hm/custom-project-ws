0944aebd75d669234203a310799260e4f5839fbf
DIGITALWARE/EdierM <EdierM@digitalware.com.co>
2023-08-04 22:20:40 -0500

    use link component instead of navigate

--- src/dashboard/components/sidebar/Sidebar.tsx
+++ src/dashboard/components/sidebar/Sidebar.tsx
@@ -1,7 +1,4 @@
-import { type MouseEvent } from 'react';
-
-import { navigate } from '@reach/router';
-
+import { NavLink } from '../../../components';
 import { links } from '../../../data';
 import { useAuthContext } from '../../../hooks';
 import { UserProfile } from '../user-profile/UserProfile';
@@ -11,11 +8,6 @@ export function Sidebar() {
     auth: { isLoggin },
   } = useAuthContext();
 
-  const handleNavigate = async (e: MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    await navigate(e.currentTarget.href);
-  };
-
   return (
     <div className='main-sidebar sidebar-style-2'>
       <aside id='sidebar-wrapper'>
@@ -24,23 +16,18 @@ export function Sidebar() {
         <ul className='sidebar-menu'>
           <li className='menu-header'>Main</li>
           {links.map(({ href, label, icon, disabled }, i) => (
-            <li
-              className='dropdown'
+            <NavLink
               key={i}
+              to={href}
+              className={`nav-link ${
+                (disabled && isLoggin) || (!disabled && !isLoggin)
+                  ? 'disabled'
+                  : ''
+              }`}
             >
-              <a
-                href={href}
-                onClick={handleNavigate}
-                className={`nav-link ${
-                  (disabled && isLoggin) || (!disabled && !isLoggin)
-                    ? 'disabled'
-                    : ''
-                }`}
-              >
-                <i className={icon}></i>
-                <span>{label}</span>
-              </a>
-            </li>
+              <i className={icon}></i>
+              <span>{label}</span>
+            </NavLink>
           ))}
         </ul>
       </aside>
